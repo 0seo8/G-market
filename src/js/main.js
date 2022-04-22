87f6ec8ce5baffa384cc7195d16355a0223552ba
0seo8 <0seo23@gmail.com>
2022-04-22 11:26:08 +0900

    feat: 슬라이드 영역 수정 전 저장

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,16 @@
 import '../stylesheet/main.scss'
 
 
-const swiper = new Swiper('.swiper-container', {
+new Swiper('.swiper-container', {
   // direction: 'horizontal', // 수평 슬라이드
   width: 976,
   autoplay: { // 자동 재생 여부
-    delay: 5000 // 5초마다 슬라이드 바뀜
+    delay: 5000 ,// 5초마다 슬라이드 바뀜
+    disableOnInteraction: false, 
   },
   loop: true, // 반복 재생 여부
-  slidesPerView: 1, // 한 번에 보여줄 슬라이드 개수
+  loopAdditionalSlides : 1,
+  // slidesPerView: 1, // 한 번에 보여줄 슬라이드 개수
   spaceBetween: 0, // 슬라이드 사이 여백
   centeredSlides: true, // 1번 슬라이드가 가운데 보이기
   pagination: { // 페이지 번호 사용 여부
