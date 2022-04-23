1c184157002287cff064112bbed91d1296d4e7bc
0seo8 <0seo23@gmail.com>
2022-04-23 14:00:58 +0900

    feat: delevery 영역 마크업

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,33 @@
 import '../stylesheet/main.scss'
 
 
-new Swiper('.swiper-container', {
-  // autoplay: { // 자동 재생 여부
-  //   delay: 4000 
-  // },
+new Swiper('.promotion .swiper-container', {
+  autoplay: { // 자동 재생 여부
+    delay: 4000 
+  },
   slidesPerView: 7, // 한 번에 보여줄 슬라이드 개수
   centeredSlides: true, // 1번 슬라이드가 가운데 보이기
   loop: true, // 반복 재생 여부
 })
+
+
+// TabLIst
+const tabItems = document.querySelectorAll('.tab-item');
+const tabContentItems = document.querySelectorAll('.tab-content-item');
+
+function selectItem(e) {
+	removeShow();
+
+	const tabContentItem = document.querySelector(`#${this.id}-content`);
+  tabContentItem.classList.add('show');
+}
+
+function removeShow() {
+	tabContentItems.forEach(item => {
+		item.classList.remove('show');
+	});
+}
+
+tabItems.forEach(item => {
+	item.addEventListener('hover', selectItem);
+});
