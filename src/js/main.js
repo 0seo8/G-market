d98a2fb65b7f29a334573cdfef5e5992a20cebf8
0seo8 <0seo23@gmail.com>
2022-04-26 14:47:34 +0900

    feat: 수정

--- src/js/main.js
+++ src/js/main.js
@@ -1,32 +1,14 @@
 // import Swiper from 'swiper'
 import '../stylesheet/main.scss'
 import  { NavSwiper, bestSwiper, delSwiper, serSwiper, chaSwiper} from './nav.js';
-import {Wrapper} from './tablist.js'
-// import{Hover} from './footer.js'
+import {selectTab, clickBtn} from './tablist.js'
+import { renderItem } from './best.js'
 
-// // SalesSerive 
-const salesService = document.querySelector('.salesService');
-const salesServiceBtn = salesService.querySelector('.salesService__btn');
-// Hover(salesService)
-
-function addActive(e) {
-  setTimeout(() => salesService.classList.add('is--active'));
-}
-
-function removeActive(e){
-  setTimeout(() => salesService.classList.remove('is--active'));
-}
-
-salesService.addEventListener('click', addActive);
-salesService.addEventListener('mouseleave', removeActive);
-salesService.addEventListener('focusin', addActive);
-salesService.addEventListener('focusout', removeActive);
-
-
-
-// delivery tablist
-Wrapper('delivery__Gmarket')
-Wrapper('delivery__smile')
+selectTab('delivery__Gmarket')
+selectTab('delivery__smile')
+selectTab('navigation')
+clickBtn()
+renderItem()
 
 //rank순위
 document.querySelectorAll('.rank').forEach((el, i) => {
--- src/js/tablist.js
+++ src/js/tablist.js
@@ -1,13 +1,17 @@
-export function Wrapper(tabParent) {
+export function selectTab(tabParent) {
   const tabItems = document.querySelectorAll(`.${tabParent} .tab`);
-  const tabContentItems = document.querySelectorAll(`.${tabParent} .offer__list`);
+  const tabContentItems = document.querySelectorAll(`.${tabParent} .tab__list`);
   const gotoLink = document.querySelector(`.${tabParent} #gotoLink`)
 
   function selectItem() {
-    gotoLink.innerText = this.innerText + ' 바로가기';
     const tabContentItem = document.querySelector(`.${tabParent} #${this.id}-content`);
     removeShow();
     tabContentItem.classList.add('show');
+    if(gotoLink) {
+      gotoLink.innerText = this.innerText + ' 바로가기';
+    } else {
+      reset()
+    }
   }
   
   function removeShow() {
@@ -15,7 +19,34 @@ export function Wrapper(tabParent) {
       item.classList.remove('show');
     });
   }
+
+  function reset() {
+    tabItems.forEach(item => {
+      item.addEventListener('focusout', removeShow )
+      item.addEventListener('mouseleave', removeShow )
+    });
+  }
   tabItems.forEach(item => {
     item.addEventListener('click', selectItem )
+    item.addEventListener('mouseover', selectItem )
   });
+}
+
+
+export function clickBtn () {
+  const btn = document.querySelector('.salesService__btn');
+
+  function addActive(e){
+    setTimeout(() => btn.classList.add('is--active'));
+  }
+  
+  function removeActive(e){
+    setTimeout(() => btn.classList.remove('is--active'));
+  }
+
+  btn.addEventListener('click', addActive )
+  btn.addEventListener('mouseenter', addActive);
+  btn.addEventListener('mouseleave', removeActive);
+  btn.addEventListener('focusin', addActive);
+  btn.addEventListener('focusout', removeActive);
 }
