73cc745d73a409b897dd20b9f95d28d0575bae73
0seo8 <0seo23@gmail.com>
2022-04-28 12:47:57 +0900

    feat: 빠진 부분 추가

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,9 @@
 import '../stylesheet/main.scss'
-import  { NavSwiper, bestSwiper, delSwiper, serSwiper, chaSwiper} from './swiper.js';
+import  { NavSwiper,bestSwiper, delSwiper, serSwiper, chaSwiper} from './swiper.js';
 import {selectTab, clickBtn} from './tablist.js';
 import { renderItem } from './best.js';
 import { goTo } from './gototop.js';
-
+import {showPro, hiddenPro } from './Btn.js'
 
 
 renderItem()
@@ -14,33 +14,19 @@ selectTab('navigation')
 clickBtn()
 goTo()
 
-const toTopEl = document.querySelector('#to-top')
-window.addEventListener('scroll', _.throttle(function () {
-  if (window.scrollY > 500) {
-    console.log('진행중?')
-    gsap.to(toTopEl, .2, {
-      x: 0
-    })
-  } else {
-    gsap.to(toTopEl, .2, {
-      x: 100
-    })
-  }}, 300))
-  
-toTopEl.addEventListener('click', function () {
-  gsap.to(window, .7, {
-  scrollTo: 0
-  })
-})
+const closeBtn = document.querySelector('.btn-close')
+const openBtn = document.querySelector('.button__more')
 
+closeBtn.addEventListener('click', showPro )
+openBtn.addEventListener('click', hiddenPro )
 
 const init = () => {
   window.addEventListener('DOMContentLoaded', () => {
       NavSwiper();
-      bestSwiper();
       delSwiper();
       serSwiper();
       chaSwiper();
+      bestSwiper();
   })
 
 }
