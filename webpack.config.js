a0d81853e93a2da7afb4b01cec92a6c9d89e433b
0seo8 <0seo23@gmail.com>
2022-05-03 13:22:59 +0900

    질문 전 커밋

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
+const os = require('os')
+
 const path = require('path')
 const HtmlPlugin = require('html-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/js/main.js',
@@ -16,7 +20,8 @@ module.exports = {
     }
   },
   module: {
-    rules: [{
+    rules: [
+      {
         test: /\.s?css$/,
         use: [
           'style-loader',
@@ -31,6 +36,15 @@ module.exports = {
           'babel-loader'
         ]
       },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        exclude: /node_modules/,
+        use: [
+          isDevMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ],
+      }
     ]
   },
   plugins: [
@@ -50,5 +64,17 @@ module.exports = {
     hints: false,
     maxEntrypointSize: 512000,
     maxAssetSize: 512000
-  }
+  },
+  optimization: {
+    // 압축
+    minimize: isDevMode ? false : true,
+    // 미니마이저
+    minimizer: [
+      // 플러그인 인스턴스 생성
+      new CssMinimizerPlugin({
+        // CPU 멀티 프로세서 병렬화 옵션 (기본 값: true)
+        parallel: os.cpus().length - 1,
+      }),
+    ],
+  },
 }
