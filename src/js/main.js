65993022ab7e57c437bac37c7df531677ea2f5ac
0seo8 <96939562+0seo8@users.noreply.github.com>
2022-11-01 20:11:56 +0900

    fix: FOUC해결

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,13 @@
-import '../stylesheet/main.scss'
-import  { NavSwiper,bestSwiper, delSwiper, serSwiper, chaSwiper} from './swiper.js';
-import { goToTop } from './gototop.js';
-import {showPro, hiddenPro } from './Btn.js'
-import { addActiveClass } from './header.js'
-import {} from './delivery.js'
-import {} from './category.js'
-
-
-
+import "../stylesheet/main.scss";
+import {
+  NavSwiper,
+  bestSwiper,
+  delSwiper,
+  serSwiper,
+  chaSwiper,
+} from "./swiper.js";
+import { goToTop } from "./gototop.js";
+import { showPro, hiddenPro } from "./Btn.js";
+import { addActiveClass } from "./header.js";
+import "./delivery.js";
+import "./category.js";
